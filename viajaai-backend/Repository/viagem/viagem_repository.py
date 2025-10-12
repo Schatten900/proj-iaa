a221a01205e45200fc95fd6c709dddbc09562b4c
Schatten900 <carloscaua.silva9@gmail.com>
2025-10-12 14:15:34 -0300

    :sparkles: Populando o banco de dados

---
+++ viajaai-backend/Repository/viagem/viagem_repository.py
@@ -0,0 +1,55 @@
+from utils.database_executor import db_executor
+
+class ViagemContainer:
+    def __init__(self):
+        pass
+
+    def cadastrarPreferencia(self,viagem_user,id_pref):
+        try:
+            QUERY = """
+                
+            """
+            params = ()
+            return db_executor.execute_insert(QUERY,params)
+        
+        except Exception as e:
+            raise Exception(f"Erro no cadastro: {str(e)}")
+
+    def cadastrarGenero(self,viagem_user,id_genero):
+        pass
+    
+    def cadastrarLazeres(self,viagem_user,id_lazer):
+        pass
+
+    def selecionar(self,id_viagem):
+        try:
+            QUERY = """
+            SELECT * FROM Viagem WHERE Id = %s
+            """
+            params = (id_viagem,)
+            return db_executor.execute_select(QUERY,params)
+
+        except Exception as e:
+            return Exception(f"Erro ao selecionar viagem: {str(e)}")
+
+    def avaliar(self,viagem_id,id_usuario,pontuacao):
+        try:
+            QUERY = """
+                INSERT INTO ViagemAvaliacao (ViagemId,UsuarioId,Avaliacao) VALUES (%s,%s,%s)
+            """
+            params = (viagem_id,id_usuario,pontuacao,)
+            db_executor.execute_insert(QUERY,params)
+
+        except Exception as e:
+            raise Exception(f"Erro ao avaliar: {str(e)}")
+        
+    def getAvaliacao(self,viagem_id):
+        try:
+            QUERY = """
+                SELECT * FROM ViagemAvaliacao WHERE ViagemId = %s
+            """
+            params = (viagem_id,)
+            db_executor.execute_select(QUERY,params)
+
+        except Exception as e:
+            raise Exception(f"Erro ao selecionar avaliacao: {str(e)}")
